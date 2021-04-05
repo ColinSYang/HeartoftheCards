4c37628fb95869c84cafc399565a20d9b7278d48
ColinSYang <70976943+ColinSYang@users.noreply.github.com>
2021-04-05 16:33:23 -0400

    Dash Updates

---
+++ Heart of the Cards/Assets/Scripts/DashCooldown.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DashCooldown : MonoBehaviour
+{
+    public Image pCooldownImage;
+
+    public Image pLockImage;
+
+    public Image pDashImage;
+
+
+    static Image cooldownImage;
+    
+    static Image lockImage;
+    
+    static Image dashImage;
+
+    void Start()
+    {
+        cooldownImage = pCooldownImage;
+        lockImage = pLockImage;
+        dashImage = pDashImage;
+        lockImage.enabled = false;
+    }
+
+    public static void UpdateCooldown(float time, float cooldown)
+    {
+        if (time >= cooldown)
+        {
+            cooldownImage.fillAmount = 0;
+            lockImage.enabled = false;
+            var fadeImageColor = dashImage.color;
+            fadeImageColor.a = 1f;
+            dashImage.color = fadeImageColor;
+        }
+        else
+        {
+            float amount = (time / cooldown);
+            cooldownImage.fillAmount = amount;
+            lockImage.enabled = true;
+            var fadeImageColor = dashImage.color;
+            fadeImageColor.a = 0.4f;
+            dashImage.color = fadeImageColor;
+        }
+    }
+}
--- Heart of the Cards/Assets/Scripts/UI/MouseSensitivityManager.cs
+++ Heart of the Cards/Assets/Scripts/UI/MouseSensitivityManager.cs
@@ -8,8 +8,8 @@ using UnityEngine.SceneManagement;
 public class MouseSensitivityManager : MonoBehaviour
 {
     public Slider sensitivitySlider;
-    public GameObject sensitivityMenu;
-    public TextMeshProUGUI sensitivtyText;
+    public GameObject menu;
+    public TextMeshProUGUI sensitivtySliderText;
 
     public static bool active;
 
@@ -19,36 +19,62 @@ public class MouseSensitivityManager : MonoBehaviour
         {
             if (active)
             {
-                active = false;
-                Time.timeScale = 1f;
-                sensitivityMenu.SetActive(false);
-                Cursor.visible = false;
-                Cursor.lockState = CursorLockMode.Locked;
+                ResumeGame();
             }
             else
             {
-                active = true;
-                Time.timeScale = 0f;
-                sensitivityMenu.SetActive(true);
-                Cursor.visible = true;
-                Cursor.lockState = CursorLockMode.None;
+                PauseGame();
             }
         }
+    }
+
+    public void PauseGame()
+    {
+        active = true;
+        Time.timeScale = 0f;
+        menu.SetActive(true);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void ResumeGame()
+    {
+        active = false;
+        Time.timeScale = 1f;
+        menu.SetActive(false);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     public void UpdateSensitivity()
     {
         LevelManager.mouseSensitivity = sensitivitySlider.value;
-        sensitivtyText.text =sensitivitySlider.value.ToString("f0");
+        sensitivtySliderText.text =sensitivitySlider.value.ToString("f0");
     }
 
     public void StartGame()
     {
+        Time.timeScale = 1f;
+        active = false;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void MainMenu()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        Time.timeScale = 1f;
+        active = false;
+        SceneManager.LoadScene(0);
+    }
+
+    public void RestartLevel()
+    {
+        Time.timeScale = 1f;
+        active = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
